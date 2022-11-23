279fa103c34007717b9a22595f13989f6d168b57
Sabika Ali <Sabikaalghadeeri@gmail.com>
2022-11-23 09:23:18 +0300

    Solved Container-Lab

--- containers-dictionary-lab/container.py
+++ containers-dictionary-lab/container.py
@@ -1,8 +1,58 @@
-# Exercise 1
-# Create a list named students containing some student names (strings).
-# Print out the second student's name.
-# Print out the last student's name.
+# Exercise1
+# students = ['Sabika','Layla', 'Ranya']
+# print(students[1])
+# print(students[2])
 
-students = ['Sabika','Layla', 'Ranya']
-print(students[1])
-print(students[2])
+
+#Exercise2
+# foods = ('pizza', 'pasta', 'sushi')
+# for food in foods:
+#     print(f"{food} is a good food")
+
+
+# Exercise3
+# for food in foods[1:3]:
+#     print(f"{food} is a good food")
+
+
+# Exercise4
+# home_town = {
+#     'city': 'Manama',
+#     'state': 'Bahrain',
+#     'population': '600,000'
+# }
+# print(f"I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}")
+
+
+# Exercise5
+# for key, value in home_town.items():
+#     print(f"{key} = {value}")
+
+
+# Exercise6
+# cohort = []
+# for idx, name in enumerate(students):
+#     cohort.append({
+#         'name': name,
+#         'fav_food': foods[idx]
+#         })
+
+# for student in cohort:
+#     print(student
+
+
+# Exercise7
+# awesome_students = []
+# for idx, name in enumerate(students):
+#     awesome_students.append({
+#         'name': name
+#         })
+
+# for student in awesome_students:
+#     print(f"{student['name']} is awesome!")
+
+
+# Exercise8
+# for food in foods:
+#     if 'a' in food:
+#         print(food)
